b2fca3fafadc053f51aaf003fb7bd7199859af65
GilianaB <giliana.b3@yandex.ru>
2022-12-09 11:57:35 +0300

    homework

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// int SumNumerals(int number)
+// {
+//     int sum = 0;
+//     for (int i=1; i<=number; i++)
+//     {
+//     sum+=i;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SumNumerals(number));
+
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Pow(a,b));
---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int SumNumerals(int number)
+{
+    int sum = 0;
+    for (int i=0; i<10; i++)
+    {
+    sum=sum+number%10;
+    number=number/10;
+    }
+    return sum+number;
+}
+
+Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumerals(number));
+
+
